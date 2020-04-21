5625c21db43993d249bac99bb3b1dd3d0d1d43f4
Theran <theran.brigowatz@gmail.com>
2020-04-21 13:47:52 +0900

    more testing

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -27,13 +27,16 @@ export default class HelloWorldSceneAR extends Component {
 
     // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
+    this._onLoadStart = this._onLoadStart.bind(this);
+    this._onLoadEnd = this._onLoadEnd.bind(this);
+    this._onError = this._onError.bind(this);
   }
 
   render() {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized}>
         <ViroNode position={[0, -1, 0]} dragType='FixedToWorld' onDrag={() => {}}>
-          <Viro3DObject
+          {/* <Viro3DObject
             source={require('./res/emoji_smile/emoji_smile.vrx')}
             resources={[
               require('./res/emoji_smile/emoji_smile_diffuse.png'),
@@ -43,8 +46,27 @@ export default class HelloWorldSceneAR extends Component {
             position={[0, 0.5, 0]}
             scale={[0.2, 0.2, 0.2]}
             type='VRX'
+          /> */}
+          <Viro3DObject
+            source={require('./look6/march12_fbx_look6_v5.vrx')}
+            resources={[
+              require('./look6/2.3cm_Edit.jpg'),
+              require('./look6/2.3cm_Edit.png'),
+              require('./look6/FCL1-PSS003-00_DIFFUSE_redred.jpg'),
+              require('./look6/FCL1-PSS003-00_DIFFUSE.jpg'),
+              require('./look6/FCL1-PSS003-00_NORMAL.png'),
+              require('./look6/FCL2-PSL002-00_brownbaby.jpg'),
+              require('./look6/FCL2-PSL002-00.png'),
+            ]}
+            position={[1, 0.5, 0]}
+            scale={[1, 1, 1]}
+            type='VRX'
+            onLoadStart={this._onLoadStart}
+            onLoadEnd={this._onLoadEnd}
+            onError={this._onError}
           />
         </ViroNode>
+
         <ViroBox position={[0, -0.5, -1]} scale={[0.3, 0.3, 0.1]} materials={['grid']} />
         <ViroAmbientLight color={'#aaaaaa'} />
         <ViroSpotLight
@@ -60,7 +82,7 @@ export default class HelloWorldSceneAR extends Component {
           scale={[0.5, 0.5, 0.5]}
           position={[0, 0, -1]}
           style={styles.helloWorldTextStyle}
-        /><Button style={styles.button}></Button>
+        />
       </ViroARScene>
     );
   }
@@ -74,6 +96,15 @@ export default class HelloWorldSceneAR extends Component {
       // Handle loss of tracking
     }
   }
+  _onLoadStart() {
+    console.log('OBJ loading has started');
+  }
+  _onLoadEnd() {
+    console.log('OBJ loading has finished');
+  }
+  _onError(event) {
+    console.log('OBJ loading failed with error: ' + event.nativeEvent.error);
+  }
 }
 
 var styles = StyleSheet.create({
@@ -85,11 +116,11 @@ var styles = StyleSheet.create({
     textAlign: 'center',
   },
   button: {
-    height: 20;
-    width: 20;
-    border-radius: 10;
+    height: 20,
+    width: 20,
+    borderRadius: 10,
     color: 'red',
-  }
+  },
 });
 
 ViroMaterials.createMaterials({

6874a801c8b26133f78263cef66bc6e6e589102e
Theran <theran.brigowatz@gmail.com>
2020-04-21 12:09:39 +0900

    first hello world

---
+++ js/HelloWorldSceneAR.js
@@ -0,0 +1,101 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  Viro3DObject,
+  ViroARPlaneSelector,
+  ViroARScene,
+  ViroAmbientLight,
+  ViroBox,
+  ViroConstants,
+  ViroMaterials,
+  ViroNode,
+  ViroSpotLight,
+  ViroText,
+} from 'react-viro';
+
+import { StyleSheet, Button } from 'react-native';
+
+export default class HelloWorldSceneAR extends Component {
+  constructor() {
+    super();
+
+    // Set initial state here
+    this.state = {
+      text: 'Initializing AR...',
+    };
+
+    // bind 'this' to functions
+    this._onInitialized = this._onInitialized.bind(this);
+  }
+
+  render() {
+    return (
+      <ViroARScene onTrackingUpdated={this._onInitialized}>
+        <ViroNode position={[0, -1, 0]} dragType='FixedToWorld' onDrag={() => {}}>
+          <Viro3DObject
+            source={require('./res/emoji_smile/emoji_smile.vrx')}
+            resources={[
+              require('./res/emoji_smile/emoji_smile_diffuse.png'),
+              require('./res/emoji_smile/emoji_smile_normal.png'),
+              require('./res/emoji_smile/emoji_smile_specular.png'),
+            ]}
+            position={[0, 0.5, 0]}
+            scale={[0.2, 0.2, 0.2]}
+            type='VRX'
+          />
+        </ViroNode>
+        <ViroBox position={[0, -0.5, -1]} scale={[0.3, 0.3, 0.1]} materials={['grid']} />
+        <ViroAmbientLight color={'#aaaaaa'} />
+        <ViroSpotLight
+          innerAngle={5}
+          outerAngle={90}
+          direction={[0, -1, -0.2]}
+          position={[0, 3, 1]}
+          color='#ffffff'
+          castsShadow={true}
+        />
+        <ViroText
+          text={this.state.text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle}
+        /><Button style={styles.button}></Button>
+      </ViroARScene>
+    );
+  }
+
+  _onInitialized(state, reason) {
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      this.setState({
+        text: 'Hello World',
+      });
+    } else if (state == ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+    }
+  }
+}
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+  button: {
+    height: 20;
+    width: 20;
+    border-radius: 10;
+    color: 'red',
+  }
+});
+
+ViroMaterials.createMaterials({
+  grid: {
+    diffuseTexture: require('./res/grid_bg.jpg'),
+  },
+});
+
+module.exports = HelloWorldSceneAR;
